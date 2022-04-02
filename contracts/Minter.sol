c62b8b97a91a544b8dff52d2a68e37840e3a64a3
Oxmellow <Oxmellow@protonmail.com>
2022-04-02 21:35:40 +0200

    Adding payment for minting and pricing mechanism

--- contracts/Minter.sol
+++ contracts/Minter.sol
@@ -9,13 +9,18 @@ contract Minter {
   NFT public myNFT;
   bytes32 immutable public root_presale_one;
   bytes32 immutable public root_presale_two;
+  uint256[] public token_id_brackets;
+  uint256[] public bracket_prices;
+  uint256 public current_bracket;
 
-
-  constructor(NFT _myNFT, bytes32 _root_presale_one, bytes32 _root_presale_two)
+  constructor(NFT _myNFT, bytes32 _root_presale_one, bytes32 _root_presale_two, uint256[] memory _bracket_prices, uint256[] memory _token_id_brackets)
   {
     myNFT = _myNFT;
     root_presale_one = _root_presale_one;
     root_presale_two = _root_presale_two;
+    require(_bracket_prices.length == _token_id_brackets.length);
+    bracket_prices = _bracket_prices;
+    token_id_brackets = _token_id_brackets;
   }
 
   function mintPresaleOne(bytes32[] calldata proof) public 
@@ -29,18 +34,14 @@ contract Minter {
     require(_verify(proof, root_presale_two, _leaf(msg.sender)), "Invalid merkle proof");
     myNFT.mint(msg.sender);
   }
-  function mint() public 
+  function mint() public payable
   {
-    myNFT.mint(msg.sender);
-  }
-
-  function multiMint(uint256 nftQuantity) public 
-  {
-    for (uint256 i = 0; i< nftQuantity; i++)
+    if (myNFT.nextTokenId() >= token_id_brackets[current_bracket])
     {
-      myNFT.mint(msg.sender);
+      current_bracket += 1;
     }
-    
+    require(msg.value >= bracket_prices[current_bracket]);
+    myNFT.mint(msg.sender);
   }
 
   function _leaf(address account)
@@ -53,6 +54,14 @@ contract Minter {
   internal pure returns (bool)
   {
       return MerkleProof.verify(proof, root, leaf);
+  }
+
+  function getPrice()
+  public
+  view
+  returns (uint256)
+  {
+    return bracket_prices[current_bracket];
   }
 
   function verifyPresaleOne(bytes32[] calldata proof, address sender)
